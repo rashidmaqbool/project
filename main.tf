2ee70a54715561b2a88a761df03d1d4fb911edff
rashidmaqbool <rashid.maqbool6207@gmail.com>
2024-08-07 11:10:47 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,14 +3,12 @@
 # Specify the Terraform provider
 provider "aws" {
   region     = var.region # Change this to your preferred AWS region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
 }
 
 # Define the EC2 instance
 resource "aws_instance" "example" {
   ami           = "ami-0aff18ec83b712f05" # Replace with a valid AMI ID
-  instance_type = "t2.micro"              # Change this to your preferred instance type
+  instance_type = var.instance_type              # Change this to your preferred instance type
 
   tags = {
     Name = "example-instance"
